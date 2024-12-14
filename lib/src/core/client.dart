498c257064ea54784c7559bde3219b20ac48be23
Maël GUERIN <mael.guerin@murena.io>
2024-12-14 16:01:12 +0100

    feat(client): add timetable related methods

--- lib/src/core/client.dart
+++ lib/src/core/client.dart
@@ -21,10 +21,12 @@ import 'package:pronote_dart/src/models/period.dart';
 import 'package:pronote_dart/src/models/session.dart';
 import 'package:pronote_dart/src/models/session_information.dart';
 import 'package:http/http.dart' as http;
+import 'package:pronote_dart/src/models/timetable.dart';
 import 'package:pronote_dart/src/models/user_parameters.dart';
 import 'package:pronote_dart/src/utils/clean_url.dart';
 
 import 'package:pronote_dart/src/models/errors/account_disabled_error.dart';
+import 'package:pronote_dart/src/utils/pronote_date.dart';
 
 class PronoteClient {
   final session = Session();
@@ -248,5 +250,64 @@ class PronoteClient {
     final response = await request.send();
 
     return GradesOverview.fromJSON(session, response.data['donnees']);
+  }
+
+  Future<Timetable> timetable(
+      [Map<String, dynamic> additional = const {}]) async {
+    final request = Request(session, 'PageEmploiDuTemps', {
+      '_Signature_': {'onglet': TabLocation.timetable.code},
+      'donnees': {
+        'estEDTAnnuel': false,
+        'estEDTPermanence': false,
+        'avecAbsencesEleve': false,
+        'avecRessourcesLibrePiedHoraire': false,
+        'avecAbsencesRessource': true,
+        'avecInfosPrefsGrille': true,
+        'avecConseilDeClasse': true,
+        'avecCoursSortiePeda': true,
+        'avecDisponibilites': true,
+        'avecRetenuesEleve': true,
+        'edt': {'G': 16, 'L': 'Emploi du temps'},
+        'ressource': session.userResource.encode(),
+        'Ressource': session.userResource.encode(),
+        ...additional
+      }
+    });
+
+    final response = await request.send();
+
+    return Timetable.fromJSON(session, response.data['donnees']);
+  }
+
+  Future<Timetable> timetableFromWeek(int weekNumber) async {
+    return await timetable(
+        {'numeroSemaine': weekNumber, 'NumeroSemaine': weekNumber});
+  }
+
+  Future<Timetable> timetableFromIntervals(DateTime startDate, DateTime? endDate) async {
+    final endDateParam = {};
+
+    if (endDate != null) {
+      final endDateMap = {
+        '_T': 7,
+        'V': encodePronoteDate(endDate)
+      };
+
+      endDateParam['dateFin'] = endDateMap;
+      endDateParam['DateFin'] = endDateMap;
+    }
+    
+    return timetable({
+      'dateDebut': {
+        '_T': 7,
+        'V': encodePronoteDate(startDate)
+      },
+      'DateDebut': {
+        '_T': 7,
+        'V': encodePronoteDate(startDate)
+      },
+
+      ...endDateParam
+    });
   }
 }
