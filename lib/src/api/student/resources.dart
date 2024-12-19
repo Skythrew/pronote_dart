4816f7e759e84f890f2d1e4a7ffce390f595c07f
Maël GUERIN <mael.guerin@murena.io>
2024-12-19 07:53:45 +0100

    feat(student): add `resourcesFromWeek` and `resourcesFromIntervals` API methods

--- lib/src/api/student/resources.dart
+++ lib/src/api/student/resources.dart
@@ -1,3 +1,4 @@
+import 'package:pronote_dart/src/api/private/student/homework.dart';
 import 'package:pronote_dart/src/core/clients/student.dart';
 import 'package:pronote_dart/src/core/request.dart';
 import 'package:pronote_dart/src/models/enums/tab_location.dart';
@@ -16,5 +17,19 @@ extension PronoteStudentResources on PronoteStudentClient {
 
     final resource = response.data['donnees']['ListeCahierDeTextes']['V'][0];
     return Resource.fromJSON(session, resource);
+  }
+
+  List<Resource> _decoder(dynamic data) {
+    return List<Resource>.from(data['ListeCahierDeTextes']['V'].map((el) => Resource.fromJSON(session, el)));
+  }
+
+  Future<List<Resource>> resourcesFromWeek(int weekNumber, [int? extendsToWeekNumber]) async {
+    final reply = await homeworkFromWeek(TabLocation.resources, weekNumber, extendsToWeekNumber);
+    return _decoder(reply);
+  }
+
+  Future<List<Resource>> resourcesFromIntervals(DateTime startDate, DateTime endDate) async {
+    final reply = await homeworkFromIntervals(TabLocation.resources, startDate, endDate);
+    return List<Resource>.from(_decoder(reply).where((el) => (startDate.isBefore(el.endDate) || startDate.isAtSameMomentAs(el.endDate)) && (endDate.isAfter(el.endDate) || endDate.isAtSameMomentAs(el.endDate))));
   }
 }
